f6fd6822668703e83c75381786c4a9a68bfc43b4
egiesem <egiesem92@gmail.com>
2017-04-18 09:34:22 +0800

    Tahap 1

---
+++ index.android.js
@@ -0,0 +1,11 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import  React from  'react';  
+                import  MainApp from  './src/MainApp';  
+                import  { AppRegistry } from  'react-native';
+
+AppRegistry.registerComponent('BrowserSaya', () => MainApp);
